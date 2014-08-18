8aa24bd8d562226d80fe7cff99ac887d8e9b3bae
Fernando Alonso <krakatoa1987@gmail.com>
2014-08-18 13:30:39 -0300

    Fix on SSML document generator

--- src/thunderl_ssml.erl
+++ src/thunderl_ssml.erl
@@ -4,6 +4,6 @@
 
 playback_node(Url) ->
   SpeakNode = <<"<speak xmlns=\"http://www.w3.org/2001/10/synthesis\" version=\"1.0\" xml:lang=\"en-US\"><audio src=\"", Url, "\"/></speak>">>,
-  {xmlel,'urn:xmpp:rayo:output:1',[],document
+  {xmlel,'urn:xmpp:rayo:output:1',[],document,
           [{xmlattr,undefined,<<"content-type">>,<<"application/ssml+xml">>}],
           [{xmlcdata,SpeakNode}]}.
